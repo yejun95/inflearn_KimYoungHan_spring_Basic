876917e660655d086003473286b336250bcbf5fa
xodlf100 <xodlf100@gmail.com>
2024-07-23 23:35:55 +0900

    Qulifier annotation 생성 및 적용

--- src/main/java/hello/core/order/OrderSerivceImpl.java
+++ src/main/java/hello/core/order/OrderSerivceImpl.java
@@ -1,5 +1,6 @@
 package hello.core.order;
 
+import hello.core.annotation.MainDiscountPolicy;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
@@ -11,17 +12,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final이 붙은것을 통해 생성자를 만들어줌 -> 21번 항목을 만들어준것
+//@RequiredArgsConstructor // final이 붙은것을 통해 생성자를 만들어줌 -> 21번 항목을 만들어준것
 public class OrderSerivceImpl implements OrderService {
 
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
-//    @Autowired
-//    public OrderSerivceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderSerivceImpl(MemberRepository memberRepository, @MainDiscountPolicy DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
