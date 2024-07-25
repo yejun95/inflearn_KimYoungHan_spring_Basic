ae5168d132d25e192eec5041daa42c84b63ae4fb
xodlf100 <xodlf100@gmail.com>
2024-07-26 01:48:46 +0900

    proxyMode 사용을 통한 가짜 빈 생성

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -1,10 +1,8 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
-import jakarta.inject.Provider;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,13 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final Provider<MyLogger> objectProvider;
+    private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestUrl = request.getRequestURI().toString();
-        MyLogger myLogger = objectProvider.get();
         myLogger.setRequestURL(requestUrl);
 
         myLogger.log("controller test");
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -1,19 +1,16 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
-import jakarta.inject.Provider;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LogDemoService {
 
-    private final Provider<MyLogger> objectProvider;
+    private final MyLogger myLogger;
 
     public void logic(String id) {
-        MyLogger myLogger = objectProvider.get();
         myLogger.log("service id = " + id);
     }
 }
